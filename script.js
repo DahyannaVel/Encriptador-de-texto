ea1532bc34ed7f0dc8391b1bf60b15479a9b4786
DahyannaVel <dalejandra.vmorales@gmail.com>
2024-08-22 19:02:22 -0500

    finalizado el encriptador

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function encriptar(){
     .replace(/a/gi, "ai")
     .replace(/e/gi, "enter")
     .replace(/i/gi, "ines")
-    .replace(/o/gi, "uber")
+    .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
 if (texto.length !=0){
     document.getElementById("texto").value = textoCifrado;
@@ -22,3 +22,30 @@ if (texto.length !=0){
     alert ("Debes ingresar algun texto");
 }
 }
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagen = document.getElementById("imagen");
+    
+    let textoCifrado = texto
+    .replace(/ai/gi, "a")
+    .replace(/enter/gi, "e")
+    .replace(/ines/gi, "i")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+if (texto.length !=0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con exito";
+        parrafo.textContent = "";
+        imagen.src = "./Assets/correcto.jpg";
+} else {
+        imagen.src = "./Assets/incorrecto.jpg";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert ("Debes ingresar algun texto");
+    }
+    }
+
